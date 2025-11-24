537f85749e655d7e57ca50a9e10b31df44fc3111
VitorAu <vitoraugustosantiago@outlook.com.br>
2025-11-24 15:00:02 -0300

    refactor shortcuts

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -20,10 +20,19 @@ return {
 			term:toggle()
 		end
 
-		vim.keymap.set("n", "th", function() toggle_term(1) end, { desc = "Toggle terminal 1", noremap = true, silent = true })
-    vim.keymap.set("n", "tj", function() toggle_term(2) end, { desc = "Toggle terminal 2", noremap = true, silent = true })
-    vim.keymap.set("n", "tk", function() toggle_term(3) end, { desc = "Toggle terminal 3", noremap = true, silent = true })
-    vim.keymap.set("n", "tl", function() toggle_term(4) end, { desc = "Toggle terminal 4", noremap = true, silent = true })
-    vim.keymap.set("n", "tt", ":ToggleTerm<CR>", { desc = "Hide terminal and exit to normal mode" })
+		vim.keymap.set("n", "th", function()
+			toggle_term(1)
+		end, { desc = "Toggle terminal 1", noremap = true, silent = true })
+		vim.keymap.set("n", "tj", function()
+			toggle_term(2)
+		end, { desc = "Toggle terminal 2", noremap = true, silent = true })
+		vim.keymap.set("n", "tk", function()
+			toggle_term(3)
+		end, { desc = "Toggle terminal 3", noremap = true, silent = true })
+		vim.keymap.set("n", "tl", function()
+			toggle_term(4)
+		end, { desc = "Toggle terminal 4", noremap = true, silent = true })
+		vim.keymap.set("n", "tt", ":ToggleTerm<CR>", { desc = "Hide terminal and exit to normal mode" })
+		vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
 	end,
 }
