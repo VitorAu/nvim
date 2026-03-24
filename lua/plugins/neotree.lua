0d1064cb987287f508e3977a96786e0cdc46663f
VitorAu <vitoraugustosantiago@outlook.com.br>
2026-03-24 18:34:16 -0300

    fix neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -10,7 +10,9 @@ return {
                     hide_dotfiles = false,
                     hide_gitignored = false
                 },
-                follow_current_file = true,
+                follow_current_file = {
+                  enabled = true,
+                },
                 use_libuv_file_watcher = true
             },
             enable_diagnostics = true,
